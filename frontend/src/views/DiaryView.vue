2d7d8622926e6b8e3e1438eaea808cd1d27fe5a0
Mareike <mareike.haug@web.de>
2022-08-05 01:09:27 +0200

    get diary data

--- frontend/src/views/DiaryView.vue
+++ frontend/src/views/DiaryView.vue
@@ -1,19 +1,36 @@
 <script lang="ts">
 
-export default {
+import {defineComponent} from "vue";
+declare interface BaseComponentData {
+  result?: Response;
+}
+
+export default defineComponent({
+  data(): BaseComponentData {
+    return {
+      result: undefined
+    }
+  },
   mounted() {
      (async () => {
-      const result = await fetch('http://localhost:3000/api/diarydata');
-      console.log(result);
+      const response = await fetch('http://localhost:3000/api/diarydata');
+       if (!response.ok) {
+         const message = `An error has occured: ${response.status}`;
+         throw new Error(message);
+       }
+       this.result = await response.json();
     })();
   }
-}
+})
 
 </script>
 
 <template>
   <div class="diary">
     <h1>Diary</h1>
+    <div>
+      {{JSON.stringify(result)}}
+    </div>
   </div>
 </template>
 
